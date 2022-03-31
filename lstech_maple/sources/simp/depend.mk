0574410798fbd691e2566f653d9210d4e245cfe2
Georgiy <you@example.com>
2022-04-01 00:11:11 +0300

    Initial solver

---
+++ lstech_maple/sources/simp/depend.mk
@@ -0,0 +1,14 @@
+/mnt/c/Users/master/CLionProjects/tmp/lstech_maple/sources/simp/SimpSolver.o /mnt/c/Users/master/CLionProjects/tmp/lstech_maple/sources/simp/SimpSolver.or /mnt/c/Users/master/CLionProjects/tmp/lstech_maple/sources/simp/SimpSolver.od /mnt/c/Users/master/CLionProjects/tmp/lstech_maple/sources/simp/SimpSolver.op: \
+ /mnt/c/Users/master/CLionProjects/tmp/lstech_maple/sources/simp/SimpSolver.cc \
+ ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
+ ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h \
+ ../mtl/Alg.h ../utils/Options.h ../utils/ParseUtils.h \
+ ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h ../utils/ccnr.h \
+ ../utils/System.h
+/mnt/c/Users/master/CLionProjects/tmp/lstech_maple/sources/simp/Main.o /mnt/c/Users/master/CLionProjects/tmp/lstech_maple/sources/simp/Main.or /mnt/c/Users/master/CLionProjects/tmp/lstech_maple/sources/simp/Main.od /mnt/c/Users/master/CLionProjects/tmp/lstech_maple/sources/simp/Main.op: \
+ /mnt/c/Users/master/CLionProjects/tmp/lstech_maple/sources/simp/Main.cc \
+ ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
+ ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
+ ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
+ ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h \
+ ../utils/ccnr.h
